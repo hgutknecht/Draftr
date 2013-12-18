198c45f7802dd10bad2242e7f9cc835be6f9c173
Hans Gutknecht <hans.gutknecht@mlssoccer.com>
2013-12-18 13:02:12 -0500

    Prevent more than one team in stage area

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,16 @@
 $(function() {
   $( "#sortable1, #sortable2, #sortable3" ).sortable({
-    connectWith: ".connectedSortable"
+    connectWith: ".connectedSortable",
+    start: function(e,ui){
+      ui.placeholder.height(ui.item.height());
+    }
   }).disableSelection();
+
+  $("#sortable1").on("sortreceive", function(event, ui) {
+    var $list = $(this);
+    if ($list.children().length > 1) {
+      $(ui.sender).sortable('cancel');
+    }
+  });
+
 });
