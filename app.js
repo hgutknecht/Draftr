f7c1cd2a3b5934132a77d42ddcc7a7de42bdb95f
Beau Sorensen <mail@beausorensen.com>
2013-12-18 13:39:02 -0500

    Adding sockjs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var http = require('http')
+  , sockjs = require('sockjs')
   , express = require('express')
   , app = express()
   , _ = require('underscore');
@@ -16,6 +17,38 @@ app.get('/', function(req, res) {
 	)
 });
 
-app.listen(3000);
+var echo = sockjs.createServer();
+
+var clients = {}
+
+echo.on('connection', function(conn) {
+  clients[conn.id] = conn
+
+  conn.on('data', function(message) {
+    var msg = JSON.parse(message)
+    send(msg, conn)
+  });
+
+  conn.on('close', function() {
+    delete clients[conn.id]
+  });
+});
+
+function send(msg, conn) {
+  var str = JSON.stringify(msg)
+  
+  for (var id in clients) {
+    // if (conn && id === conn.id) {
+    //   continue
+    // }
+    clients[id].write(str)
+  }
+}
+
+var server = http.createServer(app);
+
+echo.installHandlers(server, {prefix: '/echo'})
+
+server.listen(3000, '0.0.0.0');
 
 console.log('Express server started.');
--- public/js/client.js
+++ public/js/client.js
@@ -3,3 +3,22 @@ $(function() {
     connectWith: ".connectedSortable"
   }).disableSelection();
 });
+
+var sock = new SockJS('/echo')
+
+sock.onopen = function() {
+  console.log('open');
+};
+
+sock.onmessage = function(e) {
+  console.log('message', e);
+  var msg = JSON.parse(e.data)
+};
+
+sock.onclose = function() {
+  console.log('close');
+};
+
+function send(msg) {
+  sock.send(JSON.stringify(msg))
+}
